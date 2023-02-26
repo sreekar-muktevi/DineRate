ebe35aaa1682d99617056a3ed7076bfc3fc9ae8c
RohanRak <rohan.karamel@gmail.com>
2023-02-26 04:27:46 -0500

    Changed a lot of things. If the previous commit didn't break things, this one will for sure.

---
+++ Backend/menu.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import sqlite3
+
+def menu_to_array(campus):
+    # URL editing
+    URL = "http://menuportal.dining.rutgers.edu/foodpro/pickmenu.asp?sName=+University+Dining&locationNum=0"
+    URL += str(campus)
+
+    # Web Scraping 
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    mydivs = soup.find_all("div", {"class": "col-1"})
+
+    # Array to hold menu items
+    menu_items = []
+
+    for i in mydivs:
+        menu_items.append(str(i)[96:-6])
+
+    # Cleaning the data
+    for i in range(len(menu_items)):
+        if (menu_items[i][0] == '>'):
+            menu_items[i] = menu_items[i][1:]
+    
+    return menu_items
+    
+
+
+
+def array_to_database(conn, menu_items):
+    # Create an empty database if it doesn't already exist
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS menu_items (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            rating REAL NOT NULL
+        );
+    ''')
+
+    # Deletes database if existing already          
+    conn.execute('DELETE FROM menu_items')
+
+    # Resets index to 1
+    conn.execute('UPDATE sqlite_sequence SET seq=0 WHERE name="menu_items"')
+    conn.commit()
+
+    # Insert menu items into menu_items.db
+    for i in menu_items:
+        conn.execute('''
+            INSERT INTO menu_items (name, rating)
+            VALUES (?, ?);
+        ''', (i, 0))
+
+    conn.commit()
+    conn.close()
+
+
+# Connect to the database
+busch_conn = sqlite3.connect('DB/busch_items.db')
+livi_conn = sqlite3.connect('DB/livi_items.db')
+neilson_conn = sqlite3.connect('DB/neilson_items.db')
+brower_conn = sqlite3.connect('DB/brower_items.db')
+
+array_to_database(busch_conn, menu_to_array(4))
+array_to_database(livi_conn, menu_to_array(3))
+array_to_database(neilson_conn, menu_to_array(5))
+array_to_database(brower_conn, menu_to_array(1))
+
--- Backend/menu_scraper.py
+++ Backend/menu_scraper.py
@@ -1,74 +1,74 @@
-import requests
-from bs4 import BeautifulSoup
-import datetime
-import sqlite3
+# import requests
+# from bs4 import BeautifulSoup
+# import datetime
+# import sqlite3
 
-# Connect to the database
-conn = sqlite3.connect('DB/menu_items.db')
+# # Connect to the database
+# conn = sqlite3.connect('DB/menu_items.db')
 
-# Create an empty database if it doesn't already exist
-conn.execute('''
-    CREATE TABLE IF NOT EXISTS menu_items (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        rating REAL NOT NULL
-    );
-''')
+# # Create an empty database if it doesn't already exist
+# conn.execute('''
+#     CREATE TABLE IF NOT EXISTS menu_items (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         name TEXT NOT NULL,
+#         rating REAL NOT NULL
+#     );
+# ''')
 
-# Deletes database if existing already          
-conn.execute('DELETE FROM menu_items')
+# # Deletes database if existing already          
+# conn.execute('DELETE FROM menu_items')
 
-# Resets index to 1
-conn.execute('UPDATE sqlite_sequence SET seq=0 WHERE name="menu_items"')
-conn.commit()
+# # Resets index to 1
+# conn.execute('UPDATE sqlite_sequence SET seq=0 WHERE name="menu_items"')
+# conn.commit()
 
-# Maps each campus to the number in the link
-match (input("which campus?: ")).lower():
-    case "busch":
-        campus = 4
-    case "livi":
-        campus = 3
-    case "neilson":
-        campus = 5
-    case "brower":
-        campus = 1
+# # Maps each campus to the number in the link
+# match (input("which campus?: ")).lower():
+#     case "busch":
+#         campus = 4
+#     case "livi":
+#         campus = 3
+#     case "neilson":
+#         campus = 5
+#     case "brower":
+#         campus = 1
 
-# Future menu option 
-day = input("how many days? (0 for today, up to 7 in advance):")
+# # Future menu option 
+# day = input("how many days? (0 for today, up to 7 in advance):")
 
-# URL editing
-URL = "http://menuportal.dining.rutgers.edu/foodpro/pickmenu.asp?sName=+University+Dining&locationNum=0"
-URL += str(campus)
+# # URL editing
+# URL = "http://menuportal.dining.rutgers.edu/foodpro/pickmenu.asp?sName=+University+Dining&locationNum=0"
+# URL += str(campus)
 
-# Web Scraping 
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
-mydivs = soup.find_all("div", {"class": "col-1"})
+# # Web Scraping 
+# page = requests.get(URL)
+# soup = BeautifulSoup(page.content, "html.parser")
+# mydivs = soup.find_all("div", {"class": "col-1"})
 
-# Array to hold menu items
-menu_items = []
+# # Array to hold menu items
+# menu_items = []
 
-for i in mydivs:
-    menu_items.append(str(i)[96:-6])
+# for i in mydivs:
+#     menu_items.append(str(i)[96:-6])
 
-# Cleaning the data
-for i in range(len(menu_items)):
-    if (menu_items[i][0] == '>'):
-        menu_items[i] = menu_items[i][1:]
+# # Cleaning the data
+# for i in range(len(menu_items)):
+#     if (menu_items[i][0] == '>'):
+#         menu_items[i] = menu_items[i][1:]
 
-# Insert menu items into menu_items.db
-for i in menu_items:
-    conn.execute('''
-        INSERT INTO menu_items (name, rating)
-        VALUES (?, ?);
-    ''', (i, 0))
+# # Insert menu items into menu_items.db
+# for i in menu_items:
+#     conn.execute('''
+#         INSERT INTO menu_items (name, rating)
+#         VALUES (?, ?);
+#     ''', (i, 0))
 
 
-result_set = conn.execute('SELECT * FROM menu_items')
+# result_set = conn.execute('SELECT * FROM menu_items')
 
-# Fetch the results and print them
-for row in result_set:
-    print(row)
+# # Fetch the results and print them
+# for row in result_set:
+#     print(row)
 
-conn.commit()
-conn.close()
+# conn.commit()
+# conn.close()
