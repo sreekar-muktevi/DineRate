643dab4c649bf06f60b93f89bcfe80f6dac2ec6c
RohanRak <rohan.karamel@gmail.com>
2023-02-25 21:13:29 -0500

    m

--- scraper.py
+++ scraper.py
@@ -17,46 +17,47 @@ conn.execute('''
         rating REAL NOT NULL
     );
 ''')
-# conn.execute('DELETE FROM sqlite_sequence WHERE name="menu_items"')
-# conn.execute('ALTER TABLE menu_items AUTOINCREMENT=1')
-x = input("which campus?: ")
-if (x == "busch"):
-    x = 4
-if (x == "livi"):
-    x = 3
-if (x == "neilson"):
-    x = 5
-if (x == "brower"):
-    x = 1
-y = input("how many days? (0 for today, up to 7 in advance):")
+
+campus = 0
+
+match input("which campus?: "):
+    case "busch":
+        campus = 4
+    case "livi":
+        campus = 3
+    case "neilson":
+        campus = 2
+    case "brower":
+        campus = 1
+
+day = input("how many days? (0 for today, up to 7 in advance):")
+
 URL = "http://menuportal.dining.rutgers.edu/foodpro/pickmenu.asp?sName=+University+Dining&locationNum=0"
-URL += str(x)
-# results = soup.find(="col-1")
+URL += str(campus)
+
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
-mydivs = soup.find_all("div", {"class": "col-1"}
-                       )
-thing = []
-for i in mydivs:
-    thing.append(str(i)[96:-6])
+mydivs = soup.find_all("div", {"class": "col-1"})
 
-    
-for i in range(len(thing)):
-    if(thing[i][0] == '>'):
-        thing[i] = thing[i][1:]
-    # if(thing[i][0] == '/'):
-    #     thing.pop(i)
-    #     i -= 2
-for i in thing:
-    print(i)
+# Array to hold menu items
+menu_items = []
+
+for i in mydivs:
+    menu_items.append(str(i)[96:-6])
+
+# Cleaning the data
+for i in range(len(menu_items)):
+    if (menu_items[i][0] == '>'):
+        menu_items[i] = menu_items[i][1:]
+
+# Insert menu items into menu_items.db
+for i in menu_items:
     conn.execute('''
         INSERT INTO menu_items (name, rating)
         VALUES (?, ?);
     ''', (i, 0))
 
-
-conn.commit()
 
 result_set = conn.execute('SELECT * FROM menu_items')
 
@@ -64,4 +65,5 @@ result_set = conn.execute('SELECT * FROM menu_items')
 for row in result_set:
     print(row)
 
+conn.commit()
 conn.close()
