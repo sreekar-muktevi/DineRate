807b40259db7f5cf41c82716f94c70be7368ce97
Sreekar Muktevi <sreekarmuktevi@nbp-77-121.nbp.ruw.rutgers.edu>
2023-02-25 20:42:45 -0500

    hi

--- scraper.py
+++ scraper.py
@@ -1,23 +1,32 @@
-import database
 import requests
 from bs4 import BeautifulSoup
-import sqlite3
-
-URL = "http://menuportal.dining.rutgers.edu/foodpro/pickmenu.asp?sName=Rutgers+University+Dining&locationNum=04"
+import datetime
+x = input("which campus?: ")
+if (x == "busch"):
+    x = 4
+if (x == "livi"):
+    x = 3
+if (x == "neilson"):
+    x = 5
+if (x == "brower"):
+    x = 1
+y = input("how many days? (0 for today, up to 7 in advance):")
+URL = "http://menuportal.dining.rutgers.edu/foodpro/pickmenu.asp?sName=+University+Dining&locationNum=0"
+URL += str(x)
 # results = soup.find(="col-1")
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 mydivs = soup.find_all("div", {"class": "col-1"}
                        )
-database.menu_items
+thing = []
 for i in mydivs:
     thing.append(str(i)[96:-6])
 for i in range(len(thing)):
-    
     if(thing[i][0] == '>'):
         thing[i] = thing[i][1:]
-    if(thing[i][0] == '/'):
-        thing[i] = ''
+    # if(thing[i][0] == '/'):
+    #     thing.pop(i)
+    #     i -= 2
 for i in thing:
     print(i)
